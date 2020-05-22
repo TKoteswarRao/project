754135b2b5565e5ac853512644b7282759611a44
koti <koteswarlife@gmail.com>
2020-05-22 20:51:45 +0530

    my commit

---
+++ src/main/java/com/spring/websocket/chat/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.spring.websocket.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.spring.websocket.chat.model.ChatMessage;
+
+@Controller
+public class ChatController {
+	@MessageMapping("/chat.regitser")
+	@SendTo("/topic/public")  
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.send")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+}
